d7981ce282fed9c94dff3019a928ad6fdba9a1d1
Hao Luo <howlowck@gmail.com>
2022-09-13 17:15:16 -0500

    added dark theme toggle

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,21 +1,32 @@
-import type { Component } from "solid-js"
+import { Component, createEffect, createMemo, createSignal } from "solid-js"
 import { Link, NavLink, useMatch } from "@solidjs/router"
 import styles from "./styles.module.css"
-import logo from "../../assets/common-catalog.svg"
+import logo from "../../assets/common-catalog.svg?url"
+import Moon from "../../assets/icons-outline/moon.svg?component"
+import Sun from "../../assets/icons-outline/sun.svg?component"
+
+declare const halfmoon: any
 
 const Comp: Component = (props) => {
   const match = (path: string) => {
     return useMatch(() => path)
   }
 
+  const [isDarkMode, setIsDarkMode] = createSignal(halfmoon.darkModeOn)
+
+  createEffect(() => {
+    isDarkMode()
+    halfmoon.toggleDarkMode()
+  })
+
   return (
-    <nav class="navbar">
+    <nav class="navbar d-flex">
       <NavLink href="/" end={true} class="navbar-brand">
         <img src={logo} alt="logo" />
         Common Catalog
       </NavLink>
       <span class="navbar-text text-monospace">v1.0</span>
-      <ul class="navbar-nav d-none d-md-flex ml-auto">
+      <ul class="navbar-nav d-none d-md-flex justify-content-end flex-grow-1">
         <li class="nav-item" classList={{ active: !!match("/docs")() }}>
           <Link href="/docs" class="nav-link">
             Docs
@@ -27,6 +38,13 @@ const Comp: Component = (props) => {
           </Link>
         </li>
       </ul>
+      <button class="btn p-0 ml-5" onClick={() => setIsDarkMode(!isDarkMode())}>
+        {isDarkMode() ? (
+          <Moon style={{ stroke: "black", width: "20px", margin: "5px" }} />
+        ) : (
+          <Sun style={{ stroke: "white", width: "20px", margin: "5px" }} />
+        )}
+      </button>
       <div class="navbar-content d-md-none ml-auto">
         <div class="dropdown with-arrow">
           <button
